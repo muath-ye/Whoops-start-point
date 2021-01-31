448f8fd4aea12243276de3736e9e08bdb9447062
First Geek <48710315+first-and-geek@users.noreply.github.com>
2021-02-01 00:49:59 +0300

    Squash these changes (#5)
    * use retrun
    
    * using echo
    
    * Add composer importing comment
    
    * Update comment
    
    * see squash
    
    Co-authored-by: First Geek <first-and-geek>

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+// include composer
 require 'vendor/autoload.php';
 
-// Muathye;
+// create new instance of Whoops\Run
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
 // You may use return.
 echo Muathye;
+// see squash
